9350ce32033b37b543c2cd4932fddd9fc72a2b54
Matthew Consterdine <mrconsterdine@googlemail.com>
2020-07-26 17:22:43 +0100

    Make fractal fast and interactive

--- games/fractal/fractal.js
+++ games/fractal/fractal.js
@@ -81,51 +81,50 @@ var Fractal = {
 /* Initialization */
 Fractal.init = function(context) {
 	Fractal.State = {
-		size: {scale: 1, width: 200, height: 200, total: 200 * 200},
+		size: {scale: 1},
 		running: true,
 		generated: false,
 		rendered: false,
 		data: [],
-		scale: 64,
-		max: 64,
+		scale: 128,
+		max: 128,
 		min: 4,
 		colourMap: [],
 		image: [],
 
-		start: -2.5,    // Range of the complex plane
+		start: -2.5, // Range of the complex plane
 		stop: 1.5,
 		top: -1.6,
 		bottom: 1.6,
 
-		iterations: 500,      // Number of iterations to calculate
-		threshold: 2.0,     // Threshold
-		smooth: true,       // Fake decimal iteration count to produce smooth shading
+		iterations: 500, // Number of iterations to calculate
+		threshold: 2.0, // Threshold
+		// smooth: true, // Fake decimal iteration count to produce smooth shading
 
 		seedr: Infinity, // Set to a value other than Infinity to view a julia set
 		seedi: Infinity, // Ditto
 
-		selected: 0,       // Which fractal we are vieing (0-4) auto filled
-		order: 2,       // Order of exponentiation, negatives allowed and pretty
-		inverse: false,       // Do we inverse the base (0-1)
-		orbit: 0,       // Orbit traps (0-2)
-		region: 0        // Region splits (0-2)
+		selected: 0, // Which fractal we are viewing (0-4) auto filled
+		order: 2, // Order of exponentiation, negatives allowed and pretty
+		inverse: false, // Do we inverse the base (0-1)
+		orbit: 0, // Orbit traps (0-2)
+		region: 0, // Region splits (0-2)
 	};
 
 	context.innerHTML = '<canvas id="canvas">Your browser doesn\'t support HTML5 Canvas!</canvas>';
 	Fractal.Context = document.getElementById('canvas').getContext('2d');
+	Fractal.Context.canvas = document.getElementById('canvas');
 
 	var resizefunc = function() {
 		console.log("Resize");
 
-		Fractal.Context.canvas.width  = Fractal.Context.canvas.parentElement.clientWidth;
-		Fractal.Context.canvas.height = Fractal.Context.canvas.parentElement.clientHeight;
-
 		Fractal.State.generated = false;
 		Fractal.State.scale = Fractal.State.max;
 
-		Fractal.Context.imageData = Fractal.Context.getImageData(0, 0, Fractal.Context.canvas.width, Fractal.Context.canvas.height);
+		Fractal.Context.canvas.width  = Fractal.Context.canvas.parentElement.clientWidth / Fractal.State.scale;
+		Fractal.Context.canvas.height = Fractal.Context.canvas.parentElement.clientHeight / Fractal.State.scale;
 
-		for(var i = 0; i < Fractal.Context.imageData.data.length; i++) Fractal.Context.imageData.data[i*4 + 3] = 255;
+		Fractal.Context.imageData = Fractal.Context.getImageData(0, 0, Fractal.Context.canvas.width, Fractal.Context.canvas.height);
 
 		Fractal.Context.buffer = new ArrayBuffer(Fractal.Context.imageData.data.length);
 		Fractal.Context.buffer8 = new Uint8ClampedArray(Fractal.Context.buffer);
@@ -134,9 +133,47 @@ Fractal.init = function(context) {
 	window.addEventListener("resize", resizefunc);
 	resizefunc();
 
+	window.addEventListener("wheel", function(e) {
+		var positionX = e.x / e.target.clientWidth;
+		var positionY = e.y / e.target.clientHeight;
+		// This needs to be double the value of the keyboard zoom
+		var deltaX = (Fractal.State.stop - Fractal.State.start) / 5;
+		var deltaY = (Fractal.State.bottom - Fractal.State.top) / 5;
+		if (e.deltaY > 0) {
+			Fractal.State.start -= deltaX * positionX;
+			Fractal.State.stop += deltaX * (1 - positionX);
+			Fractal.State.top -= deltaY * positionY;
+			Fractal.State.bottom += deltaY * (1 - positionY);
+		} else if(e.deltaY < 0) {
+			Fractal.State.start += deltaX * positionX;
+			Fractal.State.stop -= deltaX * (1 - positionX);
+			Fractal.State.top += deltaY * positionY;
+			Fractal.State.bottom -= deltaY * (1 - positionY);
+		}
+		window.dispatchEvent(new Event('resize'));
+	});
+
+	var startX, startY;
+	Fractal.Context.canvas.addEventListener("mousedown", function(e) {
+		startX = e.x;
+		startY = e.y;
+	});
+
+	Fractal.Context.canvas.addEventListener("mouseup", function(e) {
+		var changeX = (e.x - startX) / e.target.clientWidth;
+		var changeY = (e.y - startY) / e.target.clientHeight;
+		var deltaX = (Fractal.State.stop - Fractal.State.start) * changeX;
+		var deltaY = (Fractal.State.bottom - Fractal.State.top) * changeY;
+		Fractal.State.start -= deltaX;
+		Fractal.State.stop -= deltaX;
+		Fractal.State.top -= deltaY;
+		Fractal.State.bottom -= deltaY;
+		window.dispatchEvent(new Event('resize'));
+	});
+
 	// Precaculate for speed.
-	Fractal.State.colourMap[-1] = Fractal.State.colourMap[undefined] = [0, 0, 0];
 	for(var i = 0; i <= Fractal.State.iterations; i++) Fractal.State.colourMap[i] = HSVtoRGB(i / 20.0, 1.0, 1.0);
+	Fractal.State.colourMap[Fractal.State.iterations] = 255 << 24;
 
 	// Start the loop
 	requestAnimationFrame(Fractal.loop);
@@ -213,18 +250,18 @@ Fractal.generate = function(x, y) {
 			}
 		} else {
 			if(modulus > t) {
-				if(Fractal.State.smooth) {
-					var temp = past.mod2();
-					var k = (t - temp) / Math.abs(temp - modulus);
-					if(k < 0) k = 0.0;
-					return i + k - 1;
-				} else {
+				// if(Fractal.State.smooth) {
+				// 	var temp = past.mod2();
+				// 	var k = (t - temp) / Math.abs(temp - modulus);
+				// 	if(k < 0) k = 0.0;
+				// 	return i + k - 1;
+				// } else {
 					return i - 1;
-				}
+				// }
 			} else if(i == Fractal.State.iterations - 1) {
 				/* Fallback if we don't reach anything. We need to fill in everything otherwise we
 				   start to see weird patterns as variables don't really have a default here */
-				return -1;
+				return Fractal.State.iterations;
 			}
 		}
 
@@ -235,42 +272,116 @@ Fractal.generate = function(x, y) {
 	return 0;
 }
 
-Fractal.events = function(state, context, res) {}
+Fractal.events = function(state, context, res) {
+	if(Keyboard.delete(37)) { // left
+		var delta = (state.stop - state.start) / 10;
+		state.start -= delta;
+		state.stop -= delta;
+		window.dispatchEvent(new Event('resize'));
+	}
+	if(Keyboard.delete(38)) { // up
+		var delta = (state.bottom - state.top) / 10;
+		state.top -= delta;
+		state.bottom -= delta;
+		window.dispatchEvent(new Event('resize'));
+	}
+	if(Keyboard.delete(39)) { // right
+		var delta = (state.stop - state.start) / 10;
+		state.start += delta;
+		state.stop += delta;
+		window.dispatchEvent(new Event('resize'));
+	}
+	if(Keyboard.delete(40)) { // down
+		var delta = (state.bottom - state.top) / 10;
+		state.top += delta;
+		state.bottom += delta;
+		window.dispatchEvent(new Event('resize'));
+	}
+	if(Keyboard.delete(61)) { // equals/plus
+		var deltaX = (state.stop - state.start) / 10;
+		var deltaY = (state.bottom - state.top) / 10;
+		state.start += deltaX;
+		state.stop -= deltaX;
+		state.top += deltaY;
+		state.bottom -= deltaY;
+		window.dispatchEvent(new Event('resize'));
+	}
+	if(Keyboard.delete(70)) { // f
+		state.selected = (state.selected + 1) % 5;
+		window.dispatchEvent(new Event('resize'));
+	}
+	if(Keyboard.delete(72)) { // h
+		state.start = -2.5;
+		state.stop = 1.5;
+		state.top = -1.6;
+		state.bottom = 1.6;
+		window.dispatchEvent(new Event('resize'));
+	}
+	if(Keyboard.delete(73)) { // i
+		state.inverse = !state.inverse;
+		window.dispatchEvent(new Event('resize'));
+	}
+	if(Keyboard.delete(79)) { // o
+		state.orbit = (state.orbit + 1) % 3;
+		window.dispatchEvent(new Event('resize'));
+	}
+	if(Keyboard.delete(80)) { // p
+		state.scale = 1;
+		state.generated = false;
+	}
+	if(Keyboard.delete(82)) { // r
+		state.region = (state.region + 1) % 3;
+		window.dispatchEvent(new Event('resize'));
+	}
+	if(Keyboard.delete(173)) { // minus/underscore
+		var deltaX = (state.stop - state.start) / 10;
+		var deltaY = (state.bottom - state.top) / 10;
+		state.start -= deltaX;
+		state.stop += deltaX;
+		state.top -= deltaY;
+		state.bottom += deltaY;
+		window.dispatchEvent(new Event('resize'));
+	}
+}
 
 /* Game update logic */
 Fractal.logic = function(state, context, res) {
-	if(!state.generated || state.scale > Fractal.State.min) {
-		state.generated = true;
-		state.rendered = false;
-		console.log("Generate");
+	if(!state.generated || state.scale > state.min) {
+		context.canvas.width  = context.canvas.parentElement.clientWidth / state.scale;
+		context.canvas.height = context.canvas.parentElement.clientHeight / state.scale;
 
-		for(var y = 0; y < context.canvas.height; y += state.scale) {
-			for(var x = 0; x < context.canvas.width; x += state.scale) {
-				var result = Math.round(Fractal.generate(x, y));
-				for(var j = 0; j < state.scale; j++) {
-					for(var i = 0; i < state.scale; i++) {
-						state.data[x+i+(y+j)*context.canvas.width] = result;
-					}
-				}
+		Fractal.Context.imageData = Fractal.Context.getImageData(0, 0, Fractal.Context.canvas.width, Fractal.Context.canvas.height);
+
+		context.buffer = new ArrayBuffer(Fractal.Context.imageData.data.length);
+		context.buffer8 = new Uint8ClampedArray(Fractal.Context.buffer);
+		context.buffer32 = new Uint32Array(Fractal.Context.buffer);
+
+		state.rendered = false;
+
+		var height = Math.max(1, Math.floor(context.canvas.height));
+		var width = Math.max(1, Math.floor(context.canvas.width));
+
+		console.log("Generate" +  width + "x" + height + " @" + state.scale);
+
+		for(var i = 0, y = 0; y < height; y += 1) {
+			for(var x = 0; x < width; i += 1, x += 1) {
+				context.buffer32[i] = state.colourMap[Math.round(Fractal.generate(x, y))];
 			}
 		}
+		context.imageData.data.set(context.buffer8);
+
+		state.generated = true;
 
 		state.timer = 0;
-		state.scale = Math.floor(state.scale/2);
+		state.scale = Math.floor(state.scale / 2);
 	}
 }
 
 /* Renderer */
 Fractal.render = function(state, context, res) {
 	if(state.rendered) return;
-	state.rendered = true;
-
-	// Cache for performance
-	var limit = context.buffer32.length;
-	for(var i = 0; i < limit; i++) context.buffer32[i] = state.colourMap[state.data[i]];
-
-	context.imageData.data.set(context.buffer8);
 	context.putImageData(context.imageData, 0, 0);
+	state.rendered = true;
 }
 
 /* Gameloop */
